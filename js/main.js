51ea34570379d0f8b2dfd38012db6073dc78560c
Alessandro <aleneri242@gmail.com>
2020-11-24 15:10:37 +0100

    aggiornamento

--- js/main.js
+++ js/main.js
@@ -1,11 +1,50 @@
-$(document).ready(function () {
+$(document).ready(
+  function () {
 
+// NOTE: Variabile freccia sinistra
 var prev = $('.fa-angle-left');
 console.log(prev);
+
+// NOTE: Varianile freccia destra
 var next = $('.fa-angle-right');
 console.log(next);
 
+$(prev).click(
+  function() {
 
+
+  }
+);
+
+
+$(next).click(
+  function() {
+
+
+  }
+);
 
 
 });
+
+
+
+
+// NOTE: FUNZIONI------------------------------------
+
+
+function prevImage() {
+
+  var imgActive = $('.images img.active');
+  console.log(imgActive);
+  var circleActive = $('.nav i.active');
+  console.log(circleActive);
+
+}
+
+
+function nextImage() {
+  var imgActive = $('.images img.active');
+  var circleActive = $('.nav i.active');
+
+}
